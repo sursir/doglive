1f6c884bc5f9d8205f7243e6415fd09b1210a7f7
Young <aisunny7.xy@gmail.com>
2015-10-27 17:30:03 +0800

    dev begin

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -7,7 +7,7 @@
  */
 class IndexController extends Yaf_Controller_Abstract {
 
-	/** 
+	/**
      * 默认动作
      * Yaf支持直接把Yaf_Request_Abstract::getParam()得到的同名参数作为Action的形参
      * 对于如下的例子, 当访问http://yourhost/Sample/index/index/index/name/young-pc\young 的时候, 你就会发现不同
